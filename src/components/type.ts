a8d5034ab92de1634a184f2391e4436ba218d9d2
svonme <svon.me@gmail.com>
2024-01-02 11:00:50 +0800

    feat: v0.4.0

--- src/components/type.ts
+++ src/components/type.ts
@@ -2,11 +2,9 @@ import type { VNode } from "vue";
 import type { FormOption } from "@ue/form";
 import type { LegacyButtonType } from "ant-design-vue/lib/button/buttonTypes";
 
-type VueNode = JSX.Element | VNode;
-
 export interface ModalProps extends FormOption{
   otherOk?: (...args: any[]) => any;
-  otherText?: string | (() => VueNode) | VueNode;
+  otherText?: string | (() => VNode) | VNode;
   otherType?: LegacyButtonType;
   buttonClassName?: string | string[];
 }
